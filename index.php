ebdfec1d61c9489511acbeda0d2e769072e2ec8a
Nicolas Bestieu <n.bestieu@gmail.com>
2026-05-15 11:53:19 +0200

    Affichage dynamique des oeuvres depuis la BDD

--- index.php
+++ index.php
@@ -4,5 +4,42 @@ require 'bdd.php';
 
 $bdd = connexion();
 
-echo "Connexion BDD OK";
+$requete = $bdd->query('SELECT * FROM oeuvres');
+
+$oeuvres = $requete->fetchAll();
+
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ArtBox</title>
+</head>
+
+<body>
+
+    <h1>Liste des œuvres</h1>
+
+    <?php foreach ($oeuvres as $oeuvre) { ?>
+
+        <div>
+
+            <h2><?php echo $oeuvre['titre']; ?></h2>
+
+            <p><?php echo $oeuvre['artiste']; ?></p>
+
+            <img src="<?php echo $oeuvre['image']; ?>" width="300">
+
+            <p><?php echo $oeuvre['description']; ?></p>
+
+        </div>
+
+        <hr>
+
+    <?php } ?>
+
+</body>
+
+</html>
